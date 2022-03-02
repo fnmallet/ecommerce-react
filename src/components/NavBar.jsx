0e094560136ff2aa5aa63a7b69ccdbd3d7eb0c11
unknown <fernandomallet@outlook.com>
2022-03-01 22:37:06 -0300

    creación del componente NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+function NavBar() {
+    return <div>
+        <nav>
+            <span>Smartech</span>
+            <ul>
+                <a href="."><li>Almacenamiento</li></a>
+                <a href="."><li>Fuentes</li></a>
+                <a href="."><li>Gabinetes</li></a>
+                <a href="."><li>Memorias</li></a>
+                <a href="."><li>Placas de Video</li></a>
+                <a href="."><li>Procesadores</li></a>
+            </ul>
+        </nav>
+    </div>;
+}
+
+export default NavBar;
